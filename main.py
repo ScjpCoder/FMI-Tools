3c3b27212ca84429cf02b5670c5a328ace9d94b8
liuyarao <1343926437@qq.com>
2022-11-10 10:05:09 +0800

    delete something.

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from fmi.emulate import FMI_polt_result, FMI_complex_simulation, FMI_simple_simu
 
 
 def simulation(filename):
-    info = FMI_version_and_type(fmu)
-    platform = FMI_platform(fmu)
+    info = FMI_version_and_type(filename)
+    platform = FMI_platform(filename)
     print(platform)
 
-    FMI_attribute(fmu)
+    FMI_attribute(filename)
 
-    desc = FMI_model_description(fmu)
+    desc = FMI_model_description(filename)
     print(desc)
     # 初始值
-    value = FMI_start_values(fmu)
+    value = FMI_start_values(filename)
     print(value)
 
-    result = FMI_simple_simulation(fmu)
+    result = FMI_simple_simulation(filename)
     print(result)
     # 设置初始值
     start_time = 0.0
     stop_time = 10.0
     step_size = 1.0
     kwargs = {
-        'filename': fmu,
+        'filename': filename,
         'start_time': start_time,
         'stop_time': stop_time,
         'fmi_type': info,
---
+++ readme.md
@@ -0,0 +1,29 @@
+# 开发环境
+开发IDE: PyCharm
+开发语言: python 3.7.9
+核心依赖: pip 22.3 、FMPy 0.3.12 、Numpy 1.21.6、pyYAML、lxml 4.9.1等等
+其他依赖:
+![](images\依赖列表.png)
+# 安装方法:
+pip install FMpy 
+自动安装大部分依赖,
+部分依赖需要手动安装
+# FmPy运行依赖:
+Visual studio build tools:
+
+下载地址:
+https://visualstudio.microsoft.com/zh-hans/visual-cpp-build-tools/
+
+安装C++桌面开发工具
+![](images\问题解决.png)
+# 运行方法:
+
+1. 复制本地模型文件路径到输入框中
+2. 回车,模型会依旧模型模型内置参数自动运行
+3. 模型运行结果会自动保存到本地的target目录下
+
+#  运行实例：
+
+fmiDemo文件夹下001-005为fmiDemo的运行实例
+
+分别取用fmiResources文件夹下的模型文件运行
